3076b0c0c5e21e92e263fdae902d02709aaefc10
unknown <you@example.com>
2023-03-23 11:03:47 +0700

    tugas

--- 17_Configuration Management and CLI/Praktikum/configcli.sh
+++ 17_Configuration Management and CLI/Praktikum/configcli.sh
@@ -12,6 +12,13 @@ mkdir "$1 at $d"/about_me/personal "$1 at $d"/about_me/professional
 #membuat file di folder
 touch "$1 at $d"/about_me/personal/facebook.txt "$1 at $d"/about_me/professional/linkedin.txt "$1 at $d"/my_friends/list_of_my_friends.txt "$1 at $d"/my_system_info/about_this_laptop.txt "$1 at $d"/my_system_info/internet_connection.txt
 
+#isi facebook argumen 2
+
+echo "https://www.facebook.com/$2" > "$1 at $d"/about_me/personal/facebook.txt
+
+#isi linkedin argumen 3
+echo "https://www.linkedin.com/in/$3" > "$1 at $d"/about_me/professional/linkedin.txt
+
 #memasukkan list of friends
 
 curl https://gist.githubusercontent.com/tegarimansyah/e91f335753ab2c7fb12815779677e914/raw/94864388379fecee450fde26e3e73bfb2bcda194/list%2520of%2520my%2520friends.txt > "$1 at $d"/my_friends/list_of_my_friends.txt
